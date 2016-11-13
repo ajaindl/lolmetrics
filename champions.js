7d595b60d6c1c9ae89e50dff81f019635ea20d47
ajaindl <ajaindl@gmail.com>
2016-11-13 01:06:09 +0000

    next commit

--- champions.js
+++ champions.js
@@ -1,87 +1,124 @@
        //globals
 
-       	var SUMMONER_NAME="thepartaker";
+
 		var API_KEY="f52fd5ba-04fa-4292-9818-3134d6148f35";
+         
 		var sumId;
 
-        $.ajax({
-            url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + SUMMONER_NAME + '?api_key=' + API_KEY,
-            type: 'GET',
-            dataType: 'json',
-            data: {
-
-            },
-            success: function (json) {
-
-            	sumId=json.SUMMONER_NAME.id;
+     
 
 
-            }
+      var getSumName= function(SUMMONER_NAME){
+            $.ajax({
+                        url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + SUMMONER_NAME + '?api_key=' + API_KEY,
+                        type: 'GET',
+                        dataType: 'json',
+                        data: {
+
+                        },
+                        success: function (SUMMONERS) {
+
+                        	sumId=SUMMONERS[SUMMONER_NAME].id;
+                              console.log(sumId);
+                              getStats(sumId);
+
+
+                        }
         });
-
-			var summonerId="67650739";
 		
-			var call=$.ajax({
-            url: 'https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/'+summonerId+'/ranked?api_key='+API_KEY,
-            type: 'GET',
-            dataType: 'json',
-            data: {
+}
 
-            },
+			
+		
+	var getStats= function(sumId){
+		$.ajax({
+                        url: 'https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/'+sumId+'/ranked?api_key='+API_KEY,
+                        type: 'GET',
+                        dataType: 'json',
+                        data: {
 
-            success: function(json){
-            	console.log(json);
-            }
-		});
-	console.log(call);
+                        },
+
+                        success: function(stats){
+                        	console.log(stats);
+                              var length=stats.champions.length;
+                              for (var i =0;i<length; i++){
+                                    getStaticData(stats.champions[i].id);
+
+                              }
+                        }
+            });
+      }
+	
 
 		var gameID;
-		var call1=$.ajax({
-            url: 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/'+summonerId+'/recent?api_key='+API_KEY,
-            type: 'GET',
-            dataType: 'json',
-            data: {
+	var getGames=function(){
+            $.ajax({
+                  url: 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/'+summonerId+'/recent?api_key='+API_KEY,
+                  type: 'GET',
+                  dataType: 'json',
+                  data: {
 
-            },
+                  },
 
-            success: function(json){
-            	console.log(json);
-            	console.log(json.games[0].gameId);
-            	var champid;
-            	for (var i=0; i<10; i++){
-            		champid=json.games[i].championId;
-            		var call=$.ajax({
-            url: 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/'+champid+'?api_key='+API_KEY,
-            type: 'GET',
-            dataType: 'json',
-            data: {
+                  success: function(game){
+                  	console.log(game);
+                  	console.log(game.games[0].gameId);
+                  	var champid=[];
+                  	for (var i=0; i<10; i++){
+                  		champid[i]=json.games[i].championId;
+                        }
+                  }
+            });
+            console.log(champid[1]);
+      }
+            		
 
-            },
+      var getStaticData=function(champid){
+            $.ajax({
+                  url: 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/'+champid+'?api_key='+API_KEY,
+                  type: 'GET',
+                  dataType: 'json',
+                  data: {
 
-            success: function(champ){
-            	console.log(champ);
-            	$('#champions').append(champ.name);
-            }
+                  },
+
+                  success: function(champ){
+                        console.log(champ);
+                  	console.log(champ.name);
+                  	$('#champions').append(champ.name).append("<br/>");
+                        gameID=champ.games[0].gameId;
+                  }
 		});
       }
-            	gameID=json.games[0].gameId;
-            			var call3=$.ajax({
-            url: 'https://na.api.pvp.net/api/lol/na/v2.2/match/'+gameID+'?api_key='+API_KEY,
-            type: 'GET',
-            dataType: 'json',
-            data: {
 
-            },
+            	
+            			
+      var getMatch=function(){
+            $.ajax({
+                  url: 'https://na.api.pvp.net/api/lol/na/v2.2/match/'+gameID+'?api_key='+API_KEY,
+                  type: 'GET',
+                  dataType: 'json',
+                  data: {
 
-            success: function(json){
-            	console.log(json);
-            }
+                  },
+
+                  success: function(match){
+                  	console.log(match);
+                  }
 		});
-            }
-		});
+      }
+	
 
+//begin dom manip
 
-$("#summoner").append(SUMMONER_NAME);
+$("#summoner_click").submit(function(event){
+      event.preventDefault();
+      var SumName=$(".form-control").val();
+      console.log(SumName);
+      getSumName(SumName);
+});
+
 	
  
 
